521a96767fbe05eaca83da7c6591a8cf1bc21075
Juan Pablo Dure <juanpablo.dure@hgtsa.com.ar>
2024-06-07 13:22:34 -0300

    Se agrega phpmyadmin al composer por comodidas

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,5 @@ Route::resource('relaciones', App\Http\Controllers\RelacioneController::class);
 Route::resource('roles', App\Http\Controllers\RoleController::class);
 
 
-##Rutas sefuras
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-//Route::get('/aulas', [App\Http\Controllers\AulaController::class, 'index'])->name('aulas');
-//Route::get('/docentes', [App\Http\Controllers\DocenteController::class, 'index'])->name('home');
-//Route::get('/comisiones', [App\Http\Controllers\ComisioneController::class, 'index'])->name('home');
-//Route::get('/carreras', [App\Http\Controllers\carreraController::class, 'index'])->name('home');
-//Route::get('/asignaturas', [App\Http\Controllers\AsignaturaController::class, 'index'])->name('home');
